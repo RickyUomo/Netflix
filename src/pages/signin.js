e0ff9fbcb8f02665c982b94aa0dca327edcac6eb
Ricky Kuo <rickykuo925@gmail.com>
2023-03-23 07:46:27 +0000

    set error

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,52 +1,47 @@
-import React, {useState} from 'react'
-import { HeaderContainer } from '../containers/header';
-import { Form } from '../components';
+import React, { useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
 export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [error, setError] = useState('')
-    const [emailAddress, setEmailAddress] = useState('')
-    const [password, setPassword] = useState('')
+  const isInvalid = emailAddress === "" || password === "";
 
-    const isInvalid = emailAddress === '' || password === '';
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
 
-    const handleSignin = event => {
-        event.preventDefault();
+  return (
+    <HeaderContainer>
+      <Form>
+        <Form.Title>Sign In</Form.Title>
 
+        <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Input
+            placeholder="Email address"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+          />
+          <Form.Input
+            type="password"
+            value={password}
+            autocomplete="off"
+            placeholder="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <Form.Submit disabled={isInvalid} type="submit">
+            Sign In
+          </Form.Submit>
 
-    }
-
-    return (
-        <HeaderContainer>
-            <Form>
-                <Form.Title>Sign In</Form.Title>
-                {error && <Form.Error>{error}</Form.Error>}
-
-                <Form.Base onSubmit={handleSignin} method="POST">
-                    <Form.Input
-                        placeholder="Email address"
-                        value={emailAddress}
-                        onChange={({ target }) => setEmailAddress(target.value)}
-                    />
-                    <Form.Input 
-                        type="password"
-                        value={password}
-                        autocomplete="off"
-                        placeholder="Password"
-                        onChange={({target}) => setPassword(target.value)}
-                    />
-                    <Form.Submit disabled={isInvalid} type="submit">
-                        Sign In
-                    </Form.Submit>
-                    
-                    <Form.Text>
-                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
-                    </Form.Text>    
-                    <Form.TextSmall>
-                        This page is protected by Google reCAPTCHA.
-                    </Form.TextSmall>
-                </Form.Base>
-            </Form>
-        </HeaderContainer>
-    )
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA.
+          </Form.TextSmall>
+        </Form.Base>
+      </Form>
+    </HeaderContainer>
+  );
 }
